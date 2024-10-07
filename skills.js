b9070f4af3eb7a67e2508588fb12660e4078e19d
Luis Ruvalcaba <lruval@microsoft.com>
2024-10-07 23:27:21 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,7 @@
+function calculateNumbers(var1, var2) {
+    console.log('Calculating numbers...');
+    console.log('var1: ' + var1);
+    console.log('var2: ' + var2);
+    console.log('var1 + var2: ' + (var1 + var2));
+    return var1 + var2;
+}
